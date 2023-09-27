c18e128335fa299bfa8ff7f737cf9bf057517e72
M-nogales <manuelnogalesserrano5221@gmail.com>
2023-09-27 13:10:57 +0200

    006

---
+++ exercise 2/006etapa.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+6.006etapa.php: A partir de una edad muestra por pantalla:
+bebé si tiene menos de 3 años
+niño si tiene entre 3 y 12 años
+adolescente entre 13 y 17 años
+adulto entre 18 y 66
+jubilado a partir de 67
+*/
+$age=33;
+$message=($age<3)?"bebé":($age<012)?"niño":($age<=17)?"adolescente":($age<=66)?"adulto":"jubilado";
+echo "$message";
+?>
